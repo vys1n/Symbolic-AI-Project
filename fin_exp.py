e971d6c6d15e50cce56455fe481e732cecd25d47
vys1n <kavysingh04@gmail.com>
2025-09-04 14:39:51 +0530

    calculations of new variables

--- fin_exp.py
+++ fin_exp.py
@@ -5,3 +5,8 @@ risk_appetite = input("What is your risk appetite? (low/medium/high): ").lower()
 savings = float(input("How much do you have in savings (in $)? "))
 debt = float(input("What is your total debt (in $)? "))
 emergency_fund = float(input("How much have you saved for emergency fund (in $)? "))
+
+savings_ratio = (savings / (income * 12)) * 100  # % of yearly income
+debt_ratio = (debt / (income * 12)) * 100        # % of yearly income
+monthly_savings = income - expenses
+months_expenses_in_emergency_fund = emergency_fund / expenses if expenses > 0 else 0
